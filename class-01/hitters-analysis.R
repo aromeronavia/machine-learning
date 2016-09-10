582104acbaae12455bb007c28bca3cc1b1d9ead1
Setzer777 <lguitarras0594@gmail.com>
2016-09-09 22:52:09 -0500

    finishes logistic regression

--- class-01/hitters-analysis.R
+++ class-01/hitters-analysis.R
@@ -1,16 +1,20 @@
-install.packages("ISLR")
 library(ISLR)
-hitters <- Hitters[!is.na(hitters$Salary), ]
-hitters$Salary = as.factor(hitters$Salary)
-meanSalary = mean(hitters$Salary)
+hitters <- Hitters[!is.na(Hitters$Salary), ]
+hitters$Salary = hitters$Salary
+meanSalary = mean(as.numeric(hitters$Salary))
 hitters$ClassSalary = 'low'
-hitters$ClassSalary[hitters$Salary < meanSalary] = 'high'
-glm.fit = glm(Salary ~ Runs + PutOuts + AtBat + Assists + Hits + Errors,
+hitters$ClassSalary[as.numeric(hitters$Salary) < meanSalary] = 'high'
+glm.fit = glm(as.factor(ClassSalary) ~ Hits + Runs + Walks,
               data = hitters,
               family = binomial)
 summary(glm.fit)
 glm.probs = predict(glm.fit, type="response")
 glm.pred = rep('low', nrow(hitters))
 glm.pred[glm.probs > .5] = 'high'
-mean(glm.pred == hitters$Salary)
-table(glm.pred, hitters$Salary)
+mean(glm.pred == hitters$ClassSalary)
+table(glm.pred, hitters$ClassSalary)
+
+train <- hitters[1: nrow(hitters) / 2, ]
+test <- hitters[(nrow(hitters) / 2) + 1: nrow(hitters), ]
+knn.reg(train, test = NULL, y, k = 3, use.all = FALSE, 
+        algorithm=c("VR", "brute", "kd_tree", "cover_tree"))
